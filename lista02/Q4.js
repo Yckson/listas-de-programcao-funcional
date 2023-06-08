e2b5acbe15b8d70d16776f8fae53984591ac7239
William Gabriel Braga <70717232+Yckson@users.noreply.github.com>
2023-06-08 18:43:05 +0000

    L2 - Q4

---
+++ lista02/Q4.js
@@ -0,0 +1,42 @@
+//Q4. Programa para classificar um triângulo em Equilátero, Isósceles ou Escaleno a partir dos valores de seus três lados.
+
+//-------------- Corpos das Funções ------------- //
+
+  function verificaTipoTri(l1, l2, l3){
+    //Esta função verifica qual o tipo de triângulo estabelecido pelo tamanho dos lados fornecidos
+    if (!verificaTri(l1, l2, l3)){
+      return "não existe!"
+    }
+    if (l1 == l2 && l2 == l3){
+      return "é equilatero!";
+    }
+    else if (l1 != l2 && l2 != l3 && l1 != l3){
+      return "é escaleno!";
+    }
+    else{
+      return "é isósceles!";
+    }
+  }
+
+  function verificaTri(ladoT1, ladoT2, ladoT3){
+    //Esta função verifica se os lados fornecidos podem compor um triângulo
+    //Para formar um triângulo válido, a soma de quaisquer dois lados deve ser maior que o terceiro lado
+    if (ladoT1 + ladoT2 > ladoT3 && ladoT2 + ladoT3 > ladoT1 && ladoT1 + ladoT3 > ladoT2){
+      return true;
+    }
+    return false;
+  }
+
+// -------------- Entrada dos Dados ------------- //
+
+  const lado1 = 4;
+  const lado2 = 4;
+  const lado3 = 4;
+
+// ---------------------------------------------- //
+// ------------- Execução da solução ------------ //
+
+  const resposta = verificaTipoTri(lado1, lado2, lado3);
+  console.log("O triângulo " + resposta);
+
+// ---------------------------------------------- //
