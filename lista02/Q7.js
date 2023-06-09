c97d041bafce667a2807400c500dbaa22fa230f9
William Gabriel Braga <70717232+Yckson@users.noreply.github.com>
2023-06-09 18:01:54 +0000

    Comentários na L2-Q7

--- lista02/Q7.js
+++ lista02/Q7.js
@@ -2,15 +2,19 @@
 //-------------- Corpos das Funções ------------- //
 
   function verificarMenor(valores){
+    //Esta função calcula o menor valor entre números quaisquer
     if (valores.length <= 1){
       return valores[0];
     }
     else{
       return menorQue(valores[0], verificarMenor(valores.slice(1)));
     }
+
+    //Para resolver este problema, foi necessário usar a recursividade das funções, onde a função invoca ela própria pra gerar uma espécie de "loop" até que a condição final seja atendida
   }
 
   function menorQue(v1, v2){
+    //Esta função retorna o menor valor entre dois números quaisquer
     if (v1 < v2) return v1;
     else return v2;
   }
